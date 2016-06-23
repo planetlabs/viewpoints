66fc138f4fb71422ca377dc58fc192bd78081b13
Matt Ferraro <matt@planet.com>
2016-06-22 19:06:34 -0700

    More messing around trying to get buttons to actuate each other

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,25 @@
 var React = require('react');
 var Dropdown = require('./dropdown');
+var Button = require('react-bootstrap').Button;
 
 var App = React.createClass({
+  onclick: function() {
+  	console.log("CLICKED DO STUFF");
+  	console.log(this.dd0);
+  	this.dd0.setState({
+  		options: ["zero", "one", "two", "three"],
+  		index: 2
+  	});
+  },
   render: function() {
-    return <Dropdown></Dropdown>;
-  }
+  	this.dd0 = <Dropdown></Dropdown>
+  	this.dd1 = <Dropdown></Dropdown>
+    return (
+    	<div>
+    		{this.dd1} vs {this.dd0}
+    		<Button onClick={this.onclick}>Do Stuff</Button>
+    	</div>);
+  },
 });
 
 module.exports = App;
--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,10 +1,29 @@
 var React = require('react');
-var Button = require('react-bootstrap').Button;
+var DropdownButton = require('react-bootstrap').DropdownButton;
+var MenuItem = require('react-bootstrap').MenuItem;
+
 
 var Dropdown = React.createClass({
-  render: function() {
-    return <Button>I am a dropdown, I guess!</Button>;
-  }
+  getInitialState() {
+    return {
+      options: ["Select Option", "Alpha", "Beta"],
+      index: 0
+    };
+  },
+  render() {
+    var createMenuItem = function(option) {
+        return <MenuItem href="#aef" onClick={this.onclick}>{option}</MenuItem>;
+    }.bind(this);
+
+    return (
+      <DropdownButton title={this.state.options[this.state.index]}>
+        {this.state.options.map(createMenuItem)}
+      </DropdownButton>
+    );
+  },
+  onclick: function() {
+    console.log("Inner clicked");
+  },
 });
 
 module.exports = Dropdown;
