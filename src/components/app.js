e280203de1cf7920f5f0c4f2a782993a5102a15c
Trevor Blades <tdblades@gmail.com>
2016-06-24 12:22:36 -0700

    Add a file uploader

--- src/components/app.js
+++ src/components/app.js
@@ -2,10 +2,24 @@ var React = require('react');
 var ViewpointsGraph = require('./viewpointsgraph');
 
 var App = React.createClass({
+
+  _onUploadChange: function(event) {
+    var reader = new FileReader();
+    reader.onload = this._onReaderLoad;
+    reader.readAsText(event.target.files[0]);
+  },
+
+  _onReaderLoad: function(event) {
+    // TODO: parse the csv
+    // var data = parseData(event.target.result);
+    // this.setState({data: data});
+  },
+
   render: function() {
     return (
       <div>
         <ViewpointsGraph options={['Alpha', 'Beta', 'Gamma']}/>
+        <input accept=".csv" onChange={this._onUploadChange} type="file"/>
       </div>);
   }
 });
